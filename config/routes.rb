dcab016f513b210ca2ea83ec6af34e07f0f4e484
Matt Gordon <mattg1520@gmail.com>
2020-06-14 13:04:55 +0300

    added devise User model

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  devise_for :users
   root to: 'application#home'
   resources :cocktails, only: [:index, :show, :new, :create, :destroy] do
     resources :doses, only: [:new, :create]
